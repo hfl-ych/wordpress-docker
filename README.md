0c7fe63079afbda1f488e0cd3523472ce4ad81a5
ych <qjsjych@qq.com>
2025-02-08 22:07:56 +0800

    config

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wordpress-docker
+
 wordpress docker 配置文件
+
+openssl req -x509 -newkey rsa:4096 -keyout localhost.key -out localhost.crt -days 365 -nodes
--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -4,18 +4,13 @@ define('DB_USER', 'wordpress_user');
 define('DB_PASSWORD', 'wordpress_pass');
 define('DB_HOST', 'db');
 
-define('AUTH_KEY',         'your-auth-key');
-define('SECURE_AUTH_KEY',  'your-secure-auth-key');
-define('LOGGED_IN_KEY',    'your-logged-in-key');
-define('NONCE_KEY',        'your-nonce-key');
-
 $table_prefix  = 'wp_';
-define('WP_DEBUG', false);
 
 if (!defined('ABSPATH')) {
     define('ABSPATH', dirname(__FILE__) . '/');
 }
 require_once ABSPATH . 'wp-settings.php';
 
-define( 'WP_HTTP_BASICAUTH', true );
+define('WP_DEBUG', false);
+define( 'WP_HTTP_BASICAUTH', false);
 define('DISABLE_WP_REST_API', false);
